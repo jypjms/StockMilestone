8a604c75a1eed2b4f5fc1a83b4002fdce6282183
jiny798 <jyoung7988@naver.com>
2023-06-09 12:11:38 +0900

    feat: 스프링부트 설정

---
+++ stock-milestone/src/main/java/fisa/stockmilestone/StockMilestoneApplication.java
@@ -0,0 +1,14 @@
+package fisa.stockmilestone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockMilestoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockMilestoneApplication.class, args);
+
+	}
+
+}
---
+++ stock-milestone/src/test/java/fisa/stockmilestone/StockMilestoneApplicationTests.java
@@ -0,0 +1,13 @@
+package fisa.stockmilestone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMilestoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
